07c190edb8deb744ee4b8a6ffd2e1fcc93b28d1a
sirsa4 <yaya.abbdi@gmail.com>
2022-02-02 21:01:06 +0100

    oppgave 2

--- oppgave5/src/App.js
+++ oppgave5/src/App.js
@@ -8,9 +8,16 @@ function App() {
   return (
     <div className="App">
       <h1>Oppgave 5</h1>
+      {/* oppgave  */}
       <div>
       <span>oppgave 1</span>
       <MyComponent/>
+      </div>
+      {/* oppgave 2 */}
+      <div>
+      <span>oppgave 2</span>
+      {/* title props added */}
+      <MyComponent title='It Works'/>
       </div>
     </div>
   );
--- oppgave5/src/compos/MyComponent.js
+++ oppgave5/src/compos/MyComponent.js
@@ -1,9 +1,15 @@
 /*
 * first letter being capital case is important for function to be recognised as component from react 
  */
-const MyComponent = ()=>{
+//props destructured in parameter
+const MyComponent = ({title})=>{
     return(
+       <div>
+        {/* oppgave 1 */}
         <h1>My First Component</h1>
+        {/* oppgave 2 */}
+        <h1>{title}</h1>
+       </div>
     )
 }
 
