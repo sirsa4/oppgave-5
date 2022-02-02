b7e9a1150be33414a4b4a768c518498c30ce8547
sirsa4 <yaya.abbdi@gmail.com>
2022-02-02 16:04:42 +0100

    oppgave 5 setup files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # oppgave 5
  Oppgave i UIN
+
+## React oppgaver
+![react logo](oppgave5/src/logo.svg)
---
+++ oppgave5/src/App.js
@@ -0,0 +1,12 @@
+//css
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Oppgave 5</h1>
+    </div>
+  );
+}
+
+export default App;
